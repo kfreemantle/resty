5647f6282cbf6527553c637f7591ae6aae2e7675
Kyle Freemantle <kcfreemantle@gmail.com>
2023-05-15 14:54:47 -0700

    lab26 done, lacking some styling

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Footer.scss'
 
-class Footer extends React.Component {
-  render() {
-    return <footer>&copy; 2018</footer>;
-  }
+function Footer() {
+  return <footer>&copy; 2023</footer>;
 }
 
 export default Footer;
+
+
+// Starter Code
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
--- src/Components/Results/index.jsx
+++ src/Components/Results/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import './Results.scss'
 
-class Results extends React.Component {
-  render() {
-    return (
-      <section>
-        <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
-      </section>
-    );
-  }
+function Results({ data }) {  // Use destructuring to get props
+  return (
+    <section>
+      <pre>{data ? JSON.stringify(data, undefined, 2) : null}</pre>
+    </section>
+  );
 }
 
 export default Results;
+
+
+// Starter code
+
+// class Results extends React.Component {
+//   render() {
+//     return (
+//       <section>
+//         <pre>{this.props.data ? JSON.stringify(this.props.data, undefined, 2) : null}</pre>
+//       </section>
+//     );
+//   }
+// }
